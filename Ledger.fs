1494cf7477a6afa0780bbe7b9f235dd25d01d076
gustav_asu4team <a.pogorelov@agroassist.ru>
2024-03-23 10:45:24 +0300

    Step2. Move format code to separate functions (with no refactoring format code itself)

--- Ledger.fs
+++ Ledger.fs
@@ -6,7 +6,61 @@ open System.Globalization
 type Entry = { dat: DateTime; des: string; chg: int }
 
 let mkEntry (date: string) description change = { dat = DateTime.Parse(date, CultureInfo.InvariantCulture); des = description; chg = change }
-        
+
+let formatDate locale (dat:DateTime) =
+    if locale = "nl-NL" then 
+        dat.ToString("dd-MM-yyyy")
+    elif locale = "en-US" then 
+        dat.ToString("MM\/dd\/yyyy")
+    else
+        failwith "Unexpected locale"
+
+let formatDesc (des:string) =
+    if des.Length <= 25 then 
+        des.PadRight(25)
+    elif des.Length = 25 then 
+        des
+    else 
+        des.[0..21] + "..."
+
+let formatChange locale currency chg =
+    let c = float chg / 100.0
+
+    if c < 0.0 then 
+        if locale = "nl-NL" then
+            if currency = "USD" then
+                ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
+            elif currency = "EUR" then
+                ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
+            else
+                failwith "Unexpected currency"
+        elif locale = "en-US" then
+            if currency = "USD" then
+                ("($" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
+            elif currency = "EUR" then
+                ("(€" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
+            else
+                failwith "Unexpected currency"
+        else
+            failwith "Unexpected locale"
+    else 
+        if locale = "nl-NL" then
+            if currency = "USD" then
+                ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
+            elif currency = "EUR" then
+                ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
+            else
+                failwith "Unexpected currency"
+        elif locale = "en-US" then
+            if currency = "USD" then
+                ("$" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
+            elif currency = "EUR" then
+                ("€" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
+            else
+                failwith "Unexpected currency"
+        else
+            failwith "Unexpected locale"
+
 let formatLedger currency locale entries =
     
     let mutable res = ""
@@ -18,44 +72,13 @@ let formatLedger currency locale entries =
 
         res <- res + Environment.NewLine
 
-        if locale = "nl-NL" then 
-            res <- res + x.dat.ToString("dd-MM-yyyy")
-
-        if locale = "en-US" then 
-            res <- res + x.dat.ToString("MM\/dd\/yyyy")
+        res <- res + formatDate locale x.dat
                 
         res <- res + " | "
 
-        if x.des.Length <= 25 then 
-            res <- res + x.des.PadRight(25)
-        elif x.des.Length = 25 then 
-            res <- res + x.des
-        else 
-            res <- res + x.des.[0..21] + "..."
+        res <- res + formatDesc x.des
 
         res <- res + " | "
-        let c = float x.chg / 100.0
 
-        if c < 0.0 then 
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL"))).PadLeft(13) 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("($" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("(€" + c.ToString("#,#0.00", new CultureInfo("en-US")).Substring(1) + ")").PadLeft(13) 
-        else 
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", new CultureInfo("nl-NL")) + " ").PadLeft(13) 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("$" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
-                if currency = "EUR" then
-                    res <- res + ("€" + c.ToString("#,#0.00", new CultureInfo("en-US")) + " ").PadLeft(13) 
+        res <- res + formatChange locale currency x.chg
     res
